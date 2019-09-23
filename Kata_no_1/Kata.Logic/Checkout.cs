ba30233b25cb6e9051f371494bcdf442ad9665ad
Pawel <pkurasz@gmail.com>
2019-09-23 08:01:38 +0100

    Passing 'Scan_OneProduct_True' test

---
+++ Kata_no_1/Kata.Logic/Checkout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.Logic
+{
+    public class Checkout
+    {
+        public List<string> Items { get; private set; }
+
+        public Checkout()
+        {
+            Items = new List<string>();
+        }
+
+
+        public void Scan(string sku)
+        {
+            Items.Add(sku);
+        }
+    }
+}
--- Kata_no_1/Kata.Tests/CheckoutUnitTests.cs
+++ Kata_no_1/Kata.Tests/CheckoutUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kata.Logic;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
