ddd0f91c2d9add03c3f21eb8e29402bd136ea27a
Pawel <pkurasz@gmail.com>
2019-09-23 07:55:34 +0100

    A test class with first failing test
    Proving that we can scan an item at a checkout

---
+++ Kata_no_1/Kata.Tests/CheckoutUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Kata.Tests
+{
+    public class CheckoutUnitTests
+    {
+        [Fact]
+        public void Scan_OneProduct_True()
+        {
+            Checkout checkout = new Checkout();
+
+            checkout.Scan("A99");
+
+            Assert.True(checkout.Items.Count == 1);
+
+        }
+    }
+}
